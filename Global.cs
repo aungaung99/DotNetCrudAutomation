5b88ea69e694c2221f844465cb8f9a655e69ad46
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-06-25 18:26:45 +0630

    Update Global.cs

--- Global.cs
+++ Global.cs
@@ -3,3 +3,6 @@ global using DotNetCrudAutomation.Model;
 global using Microsoft.EntityFrameworkCore;
 global using Scalar.AspNetCore;
 global using DotNetCrudAutomation.Data;
+global using HotChocolate;
+global using HotChocolate.AspNetCore;
+global using HotChocolate.Types;
