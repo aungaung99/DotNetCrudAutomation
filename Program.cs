1e42fd0798c8e091c55ac2cd209edbc7d47d442a
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-06-25 18:08:01 +0630

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<DotNetAutomationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
